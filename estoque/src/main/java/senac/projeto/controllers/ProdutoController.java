685c64b91c8784d5f81d5cb504212df09aac9669
jadson179 <jadson44.santos@gmail.com>
2020-10-22 21:32:33 -0300

    Criado estrutura inicial dos controllers

---
+++ estoque/src/main/java/senac/projeto/controllers/ProdutoController.java
@@ -0,0 +1,26 @@
+package senac.projeto.controllers;
+
+import java.util.ArrayList;
+
+import senac.projeto.models.vo.Produto;
+
+public class ProdutoController {
+    public void criar(Produto p) {
+    }
+
+    public void atualizar(Produto p) {
+    }
+
+    public void deletar(Produto p) {
+    }
+
+    public Produto buscarUm(Produto p) {
+
+        return null;
+    }
+
+    public ArrayList<Produto> buscarTodos(Produto p) {
+
+        return null;
+    }
+}
---
+++ estoque/src/main/java/senac/projeto/controllers/TipoLancamentoController.java
@@ -0,0 +1,26 @@
+package senac.projeto.controllers;
+
+import java.util.ArrayList;
+
+import senac.projeto.models.vo.TipoLancamento;
+
+public class TipoLancamentoController {
+    public void criar(TipoLancamento tl) {
+    }
+
+    public void atualizar(TipoLancamento tl) {
+    }
+
+    public void deletar(TipoLancamento tl) {
+    }
+
+    public TipoLancamento buscarUm(TipoLancamento tl) {
+
+        return null;
+    }
+
+    public ArrayList<TipoLancamento> buscarTodos(TipoLancamento tl) {
+
+        return null;
+    }
+}
