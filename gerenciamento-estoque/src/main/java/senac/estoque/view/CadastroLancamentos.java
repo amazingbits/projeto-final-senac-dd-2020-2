60ea2c37507d79f460516b19ac7d0989bf5cebb3
jadson179 <jadson44.santos@gmail.com>
2020-10-24 15:28:15 -0300

    Mapeando interface com a estrutura do formulário

--- gerenciamento-estoque/src/main/java/senac/estoque/view/CadastroLancamentos.java
+++ gerenciamento-estoque/src/main/java/senac/estoque/view/CadastroLancamentos.java
@@ -1,7 +1,7 @@
 package senac.estoque.view;
 
 import java.util.ArrayList;
-
+import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -18,7 +18,7 @@ public class CadastroLancamentos extends JPanel {
     private JLabel lblProduto;
     private JLabel lblSetor;
     private JLabel lblTipo;
-    private JLabel lblQuantide;
+    private JLabel lblQuantidade;
 
     private JComboBox<String> cbProduto;
     private JComboBox<String> cbSetor;
@@ -28,13 +28,54 @@ public class CadastroLancamentos extends JPanel {
     private ArrayList<SetorVO> listSetor;
     private ArrayList<TipoLancamentoVO> listTipo;
 
-    private JTextField tfQuantide;
+    private JTextField tfQuantidade;
 
     private JButton bCadastrar;
 
     public CadastroLancamentos() {
         setLayout(null);
 
+
+        lblTitle = new JLabel("CADASTRO DE PRODUTOS");
+        lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitle.setBounds(10, 11, 600, 36);
+        add(lblTitle);
+
+        lblProduto = new JLabel("Produto");
+        lblProduto.setBounds(10, 50, 600, 36);
+        add(lblProduto);
+
+        cbProduto = new JComboBox<String>();
+        cbProduto.setBounds(10, 80, 400, 36);
+        add(cbProduto);
+
+        lblSetor = new JLabel("Setor");
+        lblSetor.setBounds(420, 50, 200, 36);
+        add(lblSetor);
+
+        cbSetor = new JComboBox<String>();
+        cbSetor.setBounds(420, 80, 200, 36);
+        add(cbSetor);
+
+        lblTipo = new JLabel("Tipo");
+        lblTipo.setBounds(10, 150, 200, 36);
+        add(lblTipo);
+
+        cbTipo = new JComboBox<String>();
+        cbTipo.setBounds(10, 190, 400, 36);
+        add(cbTipo);
+
+        lblQuantidade = new JLabel("Qtde");
+        lblQuantidade.setBounds(420, 150, 200, 36);
+        add(lblQuantidade);
+
+        tfQuantidade = new JTextField();
+        tfQuantidade.setBounds(420, 190, 200, 36);
+        add(tfQuantidade);
+
+        bCadastrar = new JButton("Cadastrar");
+        bCadastrar.setBounds(10, 300, 200, 36);
+        add(bCadastrar);
 
     }
 
