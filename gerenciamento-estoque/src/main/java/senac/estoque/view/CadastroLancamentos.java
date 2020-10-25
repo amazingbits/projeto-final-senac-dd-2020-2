460882dbddbe8c666e4c17dcd1167a2963d19c47
Guilherme Andrade <gui-keanne@hotmail.com>
2020-10-25 18:02:17 -0300

    Tela de sobre a equipe

--- gerenciamento-estoque/src/main/java/senac/estoque/view/CadastroLancamentos.java
+++ gerenciamento-estoque/src/main/java/senac/estoque/view/CadastroLancamentos.java
@@ -9,7 +9,9 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 
 import senac.estoque.model.vo.LancamentoVO;
 import senac.estoque.model.vo.ProdutoVO;
@@ -35,8 +37,6 @@ public class CadastroLancamentos extends JPanel {
     private ArrayList<ProdutoVO> listProduto;
     private ArrayList<SetorVO> listSetor;
     private ArrayList<TipoLancamentoVO> listTipo;
-
-    private JTextField tfQuantidade;
 
     private JButton bCadastrar;
 
@@ -104,9 +104,14 @@ public class CadastroLancamentos extends JPanel {
         lblQuantidade = new JLabel("Qtde");
         lblQuantidade.setBounds(420, 150, 200, 36);
         add(lblQuantidade);
-
-        tfQuantidade = new JTextField();
-        tfQuantidade.setBounds(420, 190, 200, 36);
+        
+        Integer value = new Integer(1);
+        Integer min = new Integer(1);
+        Integer max = new Integer(100);
+        Integer step = new Integer(1);
+        SpinnerNumberModel spinnerModel = new SpinnerNumberModel(value, min, max, step);
+        final JSpinner tfQuantidade = new JSpinner(spinnerModel);
+        tfQuantidade.setBounds(420,191,200,36);
         add(tfQuantidade);
 
         bCadastrar = new JButton("Cadastrar");
@@ -125,7 +130,7 @@ public class CadastroLancamentos extends JPanel {
                 produtoVO.setId(listProduto.get(cbProduto.getSelectedIndex()).getId());
                 setorVO.setId(listSetor.get(cbSetor.getSelectedIndex()).getId());
                 tipoLancamentoVO.setId(listTipo.get(cbTipo.getSelectedIndex()).getId());
-                lancamentoVO.setQuantidade(Integer.parseInt(tfQuantidade.getText()));
+                lancamentoVO.setQuantidade((int)tfQuantidade.getValue());
                 lancamentoVO.setProduto(produtoVO);
                 lancamentoVO.setSetor(setorVO);
                 lancamentoVO.setTipo(tipoLancamentoVO);
---
+++ gerenciamento-estoque/src/main/java/senac/estoque/view/SobreEquipe.java
@@ -0,0 +1,142 @@
+package senac.estoque.view;
+
+import javax.swing.JPanel;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import senac.estoque.helpers.Uri;
+
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class SobreEquipe extends JPanel {
+
+	private String instagramGuilherme = "https://www.instagram.com/___guiandrade/";
+	private String githubGuilherme = "https://github.com/amazingbits";
+	private String linkedinGuilherme = "https://www.linkedin.com/in/devguiandrade/";
+	private String instagramJadson = "https://www.instagram.com/gtjadsonsantos/";
+	private String githubJadson = "https://github.com/jadson179";
+	private String linkedinJadson = "https://www.linkedin.com/in/jadson-santos-a9aa9b145/";
+	
+	/**
+	 * Create the panel.
+	 */
+	public SobreEquipe() {
+		setLayout(null);
+		
+		/**
+		 * ícone autor: Guilherme
+		 */
+		JLabel lblGuilherme = new JLabel("");
+		Image imgGuilherme = new ImageIcon(this.getClass().getResource("/gui.png")).getImage();
+		lblGuilherme.setIcon(new ImageIcon(imgGuilherme));
+		lblGuilherme.setBounds(31, 60, 120, 120);
+		add(lblGuilherme);
+		
+		JLabel lblTituloGuilherme = new JLabel("GUILHERME ANDRADE");
+		lblTituloGuilherme.setFont(new Font("Alpha Romanie G98", Font.BOLD, 12));
+		lblTituloGuilherme.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTituloGuilherme.setBounds(185, 77, 207, 30);
+		add(lblTituloGuilherme);
+		
+		JLabel lblInstagramGuilherme = new JLabel("Instagram");
+		lblInstagramGuilherme.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				Uri.openWebpage(instagramGuilherme);
+			}
+		});
+		lblInstagramGuilherme.setFont(new Font("Alpha Romanie G98", Font.PLAIN, 16));
+		lblInstagramGuilherme.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInstagramGuilherme.setBounds(167, 130, 88, 37);
+		add(lblInstagramGuilherme);
+		
+		JLabel lblGithubGuilherme = new JLabel("Github");
+		lblGithubGuilherme.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Uri.openWebpage(githubGuilherme);
+			}
+		});
+		lblGithubGuilherme.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGithubGuilherme.setFont(new Font("Alpha Romanie G98", Font.PLAIN, 16));
+		lblGithubGuilherme.setBounds(265, 130, 88, 37);
+		add(lblGithubGuilherme);
+		
+		JLabel lblLinkedinGuilherme = new JLabel("LinkedIn");
+		lblLinkedinGuilherme.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Uri.openWebpage(linkedinGuilherme);
+			}
+		});
+		lblLinkedinGuilherme.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLinkedinGuilherme.setFont(new Font("Alpha Romanie G98", Font.PLAIN, 16));
+		lblLinkedinGuilherme.setBounds(363, 130, 88, 37);
+		add(lblLinkedinGuilherme);
+		
+		/**
+		 * ícone autor: Jadson
+		 */
+		JLabel lblJadson = new JLabel("");
+		Image imgJadson = new ImageIcon(this.getClass().getResource("/jads.png")).getImage();
+		lblJadson.setIcon(new ImageIcon(imgJadson));
+		lblJadson.setBounds(415, 215, 120, 120);
+		add(lblJadson);
+		
+		JLabel lblTituloJadson = new JLabel("JADSON SANTOS");
+		lblTituloJadson.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTituloJadson.setFont(new Font("Alpha Romanie G98", Font.BOLD, 12));
+		lblTituloJadson.setBounds(167, 227, 207, 30);
+		add(lblTituloJadson);
+		
+		JLabel lblInstagramJadson = new JLabel("Instagram");
+		lblInstagramJadson.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Uri.openWebpage(instagramJadson);
+			}
+		});
+		lblInstagramJadson.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInstagramJadson.setFont(new Font("Alpha Romanie G98", Font.PLAIN, 16));
+		lblInstagramJadson.setBounds(121, 273, 88, 37);
+		add(lblInstagramJadson);
+		
+		JLabel lblGithubJadson = new JLabel("Github");
+		lblGithubJadson.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Uri.openWebpage(githubJadson);
+			}
+		});
+		lblGithubJadson.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGithubJadson.setFont(new Font("Alpha Romanie G98", Font.PLAIN, 16));
+		lblGithubJadson.setBounds(219, 273, 88, 37);
+		add(lblGithubJadson);
+		
+		JLabel lblLinkedinJadson = new JLabel("LinkedIn");
+		lblLinkedinJadson.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Uri.openWebpage(linkedinJadson);
+			}
+		});
+		lblLinkedinJadson.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLinkedinJadson.setFont(new Font("Alpha Romanie G98", Font.PLAIN, 16));
+		lblLinkedinJadson.setBounds(317, 273, 88, 37);
+		add(lblLinkedinJadson);
+		
+		JLabel lblTitulo = new JLabel("A EQUIPE");
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Alpha Romanie G98", Font.BOLD, 12));
+		lblTitulo.setBounds(185, 11, 207, 30);
+		add(lblTitulo);
+
+	}
+
+}
